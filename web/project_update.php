f3e06b120ae2996e70e04caf630f9333a226f93c
Mike Abbott-Whitley <mabbottwhitley@gmail.com>
2013-03-30 12:36:12 -0700

    Added post check to project_update.php

--- web/project_update.php
+++ web/project_update.php
@@ -6,35 +6,42 @@
 	ini_set('display_errors',1); 
 	error_reporting(E_ALL);
 
-	$project_id = $_POST["project_id"];
-	$name = $_POST["name"];
-	$owner = $_POST["owner"];
-	$start = $_POST["start"];
-	$due = $_POST["due"];
-	$progress = $_POST["progress"];
-	$note = $_POST["note"];
+	if (isset($_POST["project_id"])){
 
-	$newProject = new Project($name, $owner, $start, $due, $progress, $note);
+		$project_id = $_POST["project_id"];
+		$name = $_POST["name"];
+		$owner = $_POST["owner"];
+		$start = $_POST["start"];
+		$due = $_POST["due"];
+		$progress = $_POST["progress"];
+		$note = $_POST["note"];
 	
-	$name = $newProject->getName();
-	$owner = $newProject->getOwner();
-	$start = $newProject->getStart();
-	$due = $newProject->getDue();
-	$progress = $newProject->getProgress();
-	$note = $newProject->getNote();
-	
-	$query = "UPDATE project SET project_name='$name', owner_id='$owner', start_date='$start', due_date='$due', progress='$progress', notes='$note' WHERE project_id='$project_id'";
+		$newProject = new Project($name, $owner, $start, $due, $progress, $note);
 		
-	$r = mysqli_query ($dbc, $query); 
+		$name = $newProject->getName();
+		$owner = $newProject->getOwner();
+		$start = $newProject->getStart();
+		$due = $newProject->getDue();
+		$progress = $newProject->getProgress();
+		$note = $newProject->getNote();
 		
-	//check to ensure the query executed correctly 
+		$query = "UPDATE project SET project_name='$name', owner_id='$owner', start_date='$start', due_date='$due', progress='$progress', notes='$note' WHERE project_id='$project_id'";
+			
+		$r = mysqli_query ($dbc, $query); 
+			
+		//check to ensure the query executed correctly 
+			
+		if($r){
 		
-	if($r){
-	
-		echo "Project Updated Successfully";
+			echo "Project Updated Successfully";
+			
+		}else {
+			echo "Project not updated";
+		}
+	}else{
 		
-	}else {
-		echo "Project not updated";
+		echo "Sorry, you must have reached this page in error.";
+		
 	}
 
 
